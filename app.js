3dab9ef549746510fea0a70f8bd9a5c5afff102b
VishvnathBatra <159440635+VishvnathBatra@users.noreply.github.com>
2024-02-24 01:49:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+/* Q3:
+      create marksheet using if else and else if condition ? */
+
+//       var name = prompt("Enter your name");
+// var english = +prompt("Enter English marks");
+// var math = +prompt("Enter Math marks");
+// var chemistry = +prompt("Enter Chemistry marks");
+// var biology = +prompt("Enter Biology marks");
+// var js = +prompt("Enter JavaScript marks");
+
+// var total = english + math + chemistry + biology + js;
+// var percentage = (total / 500) * 100;
+
+// if (percentage >= 90 && percentage <= 100) {
+//     console.log(name + "  " + " your grade is A+" + "  " + percentage + "%");
+// } else if (percentage >= 70 && percentage <= 89) {
+//     console.log(name + "   " + "Your Grade Is A" + "  " + percentage + "%");
+// } else if (percentage >= 60 && percentage <= 69) {
+//     console.log(name + "  " + " your grade is B " + "  " + percentage + "%");
+// } else if (percentage >= 50 && percentage <= 59) {
+//     console.log(name + "  " + " your grade is C " + "  " + percentage + "%");
+// } else {
+//     console.log(name + "  " + "your grade is fail" + "  " + percentage + "%");
+// }
+
+
+       //=========================================================================================================
+
+
+
+/* Q2:
+       Get user input a number using prompt () check if the number is muultipale of 6 ? */
+
+//        let num = +prompt(" Enter Your Number")
+
+//  if( num%6 === 0){
+//        document.write( "Your Number Is Multiple Of 6")
+//  }
+// else{
+//        document.write( "Your Number Is Not Multiple Of 6")}   
+//   //===========================================================================================================
+
+/*Q1:
+     Write a code which can give grades to student according to their scores*/
+
+// var englishScore = +prompt("Enter English score:");
+// var mathScore = +prompt("Enter Math score:");
+// var scienceScore = +prompt("Enter Science score:");
+// var historyScore = +prompt("Enter History score:");
+// var geographyScore = +prompt("Enter Geography score:");
+
+// var totalScore = englishScore + mathScore + scienceScore + historyScore + geographyScore;
+// var averageScore = totalScore / 5;
+// var grade;
+// if (averageScore >= 90 && averageScore <= 100) {
+//     grade = "A+";
+// } else if (averageScore >= 80 && averageScore < 90) {
+//     grade = "A";
+// } else if (averageScore >= 70 && averageScore < 80) {
+//     grade = "B";
+// } else if (averageScore >= 60 && averageScore < 70) {
+//     grade = "C";
+// } else if (averageScore >= 50 && averageScore < 60) {
+//     grade = "D";
+// } else if (averageScore >= 0 && averageScore < 50) {
+//     grade = "F";
+// } else {
+//     grade = "Invalid Score";
+// }
+// console.log("Average Score: " + averageScore);
+// console.log("Grade: " + grade);
